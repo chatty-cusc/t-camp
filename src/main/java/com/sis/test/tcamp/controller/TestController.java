081aa6e696f8869f379d2262480f882fa801634b
chendian <chendian@sis.sh.cn>
2023-04-28 12:24:49 +0800

    修改外部依赖路径

--- src/main/java/com/sis/test/tcamp/controller/TestController.java
+++ src/main/java/com/sis/test/tcamp/controller/TestController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
 
@@ -13,13 +15,27 @@ import java.util.List;
 public class TestController {
 
     @GetMapping(value = "/test")
-    public List<String> test(){
-        return ShellUtils.executeNewFlow(Collections.singletonList("echo -n \"hello tongsuo\" | lib/bin/tongsuo dgst -sm3"));
+    public List<String> test() throws IOException {
+        File directory = new File("src/main/resources");
+        String reportPath = directory.getCanonicalPath();
+        String resource =reportPath + "/lib/bin/tongsuo";
+        System.out.println(resource);
+        return ShellUtils.executeNewFlow(Collections.singletonList("echo -n \"hello tongsuo\" | "+resource+" dgst -sm3"));
     }
 
 
     @GetMapping(value = "/test2")
     public List<String> test2(){
         return ShellUtils.executeNewFlow(Collections.singletonList("echo -n \"hello tongsuo\" | /opt/tongsuo/bin/tongsuo dgst -sm3"));
+    }
+
+
+    @GetMapping(value = "/test3")
+    public List<String> test3() throws IOException {
+        File directory = new File("src/main/resources");
+        String reportPath = directory.getCanonicalPath();
+        String resource =reportPath + "/lib/bin/tongsuo";
+        System.out.println(resource);
+        return ShellUtils.executeNewFlow(Collections.singletonList("echo -n \"hello tongsuo\" | /src/main/resources/lib/bin/tongsuo dgst -sm3"));
     }
 }
