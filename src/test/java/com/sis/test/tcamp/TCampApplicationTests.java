264fdce5a377d149113c853fd4492d1032832fee
chendian <chendian@sis.sh.cn>
2023-04-28 15:33:50 +0800

    修改执行脚本

--- src/test/java/com/sis/test/tcamp/TCampApplicationTests.java
+++ src/test/java/com/sis/test/tcamp/TCampApplicationTests.java
@@ -6,8 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class TCampApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+    public static void main(String[] args) {
 
+    }
 }
